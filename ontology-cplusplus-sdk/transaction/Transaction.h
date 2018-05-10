708d5bf480fb14d2bb99bb39962db742bd8786ee
wdx7266 <wdx7266@vip.qq.com>
2018-05-10 21:20:06 +0800

    update Transaction.h

--- ontology-cplusplus-sdk/transaction/Transaction.h
+++ ontology-cplusplus-sdk/transaction/Transaction.h
@@ -1,20 +1,22 @@
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
 
-#include <boost/multiprecision/cpp_int.hpp>
+#include "Attribute.h"
 #include "TransactionType.h"
+#include <boost/multiprecision/cpp_int.hpp>
+#include <vector>
+
 using namespace boost::multiprecision;
 
-class Transaction
-{
-    uint8_t version = 0;
-    TransactionType txType;
-    // int nonce = new Random().nextInt();
-    int nonce;
-    Attribute[] attributes;
-    Fee[] fee = new Fee[0];
-    long networkFee;
-    Sig[] sigs = new Sig[0];
+class Transaction {
+  uint8_t version = 0;
+  TransactionType txType;
+  // int nonce = new Random().nextInt();
+  int nonce;
+  std::vector<Attribute> attributes;
+  // Fee[] fee = new Fee[0];
+  long long networkFee;
+  // Sig[] sigs = new Sig[0];
 };
 
 #endif // !TRANSACTION_H
