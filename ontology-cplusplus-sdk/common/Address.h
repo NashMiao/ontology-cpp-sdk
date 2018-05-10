85be9e4c144b2d5d358e460434c459f2f3db1c5f
wdx7266 <wdx7266@vip.qq.com>
2018-05-10 10:52:04 +0800

    add Address.h

--- ontology-cplusplus-sdk/common/Address.h
+++ ontology-cplusplus-sdk/common/Address.h
@@ -1,8 +1,11 @@
 #ifndef ADDRESS_H
 #define ADDRESS_H
 
-class Address{
+#include <boost/multiprecision/cpp_int.hpp>
+using namespace boost::multiprecision;
 
+class Address{
+    u_int8_t COIN_VERSION = 0x41;
 };
 
 
