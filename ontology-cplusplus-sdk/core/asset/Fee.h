6fe7379c48afcdb9d09db518155a4320dde56f8a
wdx7266 <wdx7266@vip.qq.com>
2018-05-11 18:01:21 +0800

    update Fee.h

--- ontology-cplusplus-sdk/core/asset/Fee.h
+++ ontology-cplusplus-sdk/core/asset/Fee.h
@@ -2,16 +2,26 @@
 #define FEE_H
 
 #include "../../common/Address.h"
+#include "../../io/BinaryReader.h"
+#include "../../io/BinaryWriter.h"
 
 class Fee
 {
-  public:
-    long long amount;
-    Address payer;
-    Fee(long long &amount, Address &payer)
-    {
-        amount = amount;
-        payer = payer;
-    }
+private:
+  long long amount;
+  Address payer;
+
+public:
+  Fee(long long &amount, Address &payer)
+  {
+    amount = amount;
+    payer = payer;
+  }
+
+  void deserialize(BinaryReader &reader)
+  {
+    amount = reader.readLong();
+    payer.deserialize(reader);
+  }
 };
 #endif // !FEE_H
