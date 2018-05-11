4f13c67dfeb217d9c61e0af10fc0b362136d305c
wdx7266 <wdx7266@vip.qq.com>
2018-05-11 17:59:10 +0800

    update Attribute

--- ontology-cplusplus-sdk/core/transaction/Attribute.h
+++ ontology-cplusplus-sdk/core/transaction/Attribute.h
@@ -1,18 +1,29 @@
 #ifndef ATTRIBUTE_H
 #define ATTRIBUTE_H
 
-#include <boost/multiprecision/cpp_int.hpp>
-#include <vector>
+#include "../../io/BinaryReader.h"
 #include "AttributeUsage.h"
+#include <string>
+#include <vector>
 
 using namespace boost::multiprecision;
 
-class Attribute
-{
-  public:
-    AttributeUsage usage;
-    std::vector<uint8_t> data;
-    int size;
+class Attribute {
+private:
+  AttributeUsage usage;
+  std::string data;
+  int size;
+
+public:
+  void deserialize(BinaryReader& reader) {
+    usage = valueOf(reader.readByte());
+    if (usage == Script || usage == DescriptionUrl || usage == Description ||
+        usage == Nonce) {
+      data = reader.readVarBytes(255);
+    } else {
+      throw "IOException";
+    }
+  }
 };
 
 #endif // !ATTRIBUTE_H
--- ontology-cplusplus-sdk/core/transaction/AttributeUsage.h
+++ ontology-cplusplus-sdk/core/transaction/AttributeUsage.h
@@ -8,4 +8,25 @@ enum AttributeUsage {
   Description = 0x90
 };
 
+AttributeUsage valueOf(int v) {
+  AttributeUsage ret;
+  switch (v) {
+  case 0x00:
+    ret = Nonce;
+    break;
+  case 0x20:
+    ret = Script;
+    break;
+  case 0x81:
+    ret = DescriptionUrl;
+    break;
+  case 0x90:
+    ret = Description;
+    break;
+  default:
+    throw "IllegalArgumentException";
+  }
+  return ret;
+}
+
 #endif // !ATTRIBUTEUSAGE_H
