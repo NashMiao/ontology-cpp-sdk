7f9718d036086b3806cf8aa2dc9f542f1e6f75f2
wdx7266 <wdx7266@vip.qq.com>
2018-05-11 12:00:14 +0800

    update OntAssetTx.h

---
+++ ontology-cplusplus-sdk/sdk/manager/OntAssetTx.h
@@ -0,0 +1,22 @@
+#ifndef ONTASSETTX_H
+#define ONTASSETTX_H
+
+#include <string>
+
+class OntAssetTx
+{
+  private:
+    // OntSdk sdk;
+    std::string ontContract;
+    std::string ongContract;
+    int precision;
+
+  public:
+    OntAssetTx()
+    {
+        ontContract = "ff00000000000000000000000000000000000001";
+        ongContract = "ff00000000000000000000000000000000000002";
+        precision = 1;
+    }
+};
+#endif // !ONTASSETTX_H
