4c948743824007471081704dc6c39b0ccd764b10
wdx7266 <wdx7266@vip.qq.com>
2018-05-11 20:00:56 +0800

    fix bug

--- ontology-cplusplus-sdk/core/transaction/Transaction.h
+++ ontology-cplusplus-sdk/core/transaction/Transaction.h
@@ -2,8 +2,8 @@
 #define TRANSACTION_H
 
 #include "../../core/asset/Fee.h"
-#include "../../io/BinaryReader.h"
-#include "../../io/BinaryWriter.h"
+#include "../asset/Sig.h"
+#include "Attribute.h"
 #include "TransactionType.h"
 #include <nlohmann/json.hpp>
 
@@ -11,37 +11,96 @@
 
 class Transaction
 {
-  public:
-    unsigned char version;
-    TransactionType txType;
-    int nonce;
-    //   std::vector<Attribute> attributes;
-    std::vector<Fee> fee;
-    long long networkFee;
-    //   std::vector<Sig> sigs;
-    Transaction()
+private:
+  unsigned int version;
+  TransactionType txType;
+  int nonce;
+  std::vector<Attribute> attributes;
+  std::vector<Fee> fee;
+  long long networkFee;
+  std::vector<Sig> sigs;
+
+public:
+  Transaction(TransactionType type)
+  {
+    // int nonce = new Random().nextInt();
+    // Fee[] fee = new Fee[0];
+    // Sig[] sigs = new Sig[0];
+    version = 0;
+    txType = type;
+  }
+
+  // void json_out()
+  // {
+  //   nlohmann::json Result;
+  //   Result["version"] = version;
+  //   Result["TxType"] = txType;
+  //   Result["Nonce"] = nonce;
+  //   Result["Attributes"] = attributes;
+  //   Result["Fee"] = fee;
+  //   Result["NetworkFee"] = networkFee;
+  //   Result["Sigs"] = sigs;
+  //   cout << "Result:\n"
+  //        << Result << endl;
+  // }
+
+  void deserialize()
+  {
+    std::string result =
+        "00d1ee68fdec0000000000000000807d67000080b0cc71bda8653599c5666cae084bff"
+        "587e2de10064231202032fac97c3c721c437fe310b5d8e075c6e925d2de59d0713078a"
+        "ef28b70287ab612a6469643a6f6e743a5441345a63724845427a683557686d4d4e6e38"
+        "6f68353357543368755a65316a767452c1125265674964576974685075626c69634b65"
+        "79000000000000000000000101231202032fac97c3c721c437fe310b5d8e075c6e925d"
+        "2de59d0713078aef28b70287ab6101014101417f393dfe2ca8fc9fbee036dce56142b6"
+        "176f510359b3cf70164da14c83dfb0f21bd5157c141463f622a84bfd631a016bf17b3a"
+        "8a835081a67601a7bc0b9bd3";
+    BinaryReader reader;
+    reader.set_uc_vec(result);
+
+    deserializeUnsigned(reader);
+    int sign_len = (int)reader.readVarInt(0x10000000);
+    for (int i = 0; i < sign_len; i++)
     {
-        // int nonce = new Random().nextInt();
-        // Fee[] fee = new Fee[0];
-        // Sig[] sigs = new Sig[0];
-        version = 0;
+      Sig t_sig;
+      t_sig.deserialize(reader);
+      sigs.push_back(t_sig);
     }
-    void deserialize()
+  }
+
+  void deserializeUnsigned(BinaryReader &reader)
+  {
+    version = reader.readByte();
+    TransactionType t_txtype;
+    t_txtype = TxTypeDeserialize(reader);
+    if (txType != t_txtype)
     {
-        std::string result =
-            "00d1ee68fdec0000000000000000807d67000080b0cc71bda8653599c5666cae084bff"
-            "587e2de10064231202032fac97c3c721c437fe310b5d8e075c6e925d2de59d0713078a"
-            "ef28b70287ab612a6469643a6f6e743a5441345a63724845427a683557686d4d4e6e38"
-            "6f68353357543368755a65316a767452c1125265674964576974685075626c69634b65"
-            "79000000000000000000000101231202032fac97c3c721c437fe310b5d8e075c6e925d"
-            "2de59d0713078aef28b70287ab6101014101417f393dfe2ca8fc9fbee036dce56142b6"
-            "176f510359b3cf70164da14c83dfb0f21bd5157c141463f622a84bfd631a016bf17b3a"
-            "8a835081a67601a7bc0b9bd3";
-        BinaryReader BinRead;
-        BinRead.set_uc_vec(result);
-        nlohmann::json Result;
-        int version = BinRead.readInt();
-        Result["version"] = version;
+      throw "IOException";
     }
+    deserializeUnsignedWithoutType(reader);
+  }
+
+  void deserializeUnsignedWithoutType(BinaryReader &reader)
+  {
+    deserializeExclusiveData(reader);
+    int vec_len = (int)reader.readVarInt(0x10000000);
+    for (int i = 0; i < vec_len; i++)
+    {
+      Attribute t_attr;
+      t_attr.deserialize(reader);
+      attributes.push_back(t_attr);
+    }
+
+    int fee_len = (int)reader.readVarInt();
+    for (int i = 0; i < fee_len; i++)
+    {
+      Fee t_fee;
+      t_fee.deserialize(reader);
+      fee.push_back(t_fee);
+    }
+    networkFee = reader.readLong();
+  }
+
+  void deserializeExclusiveData(BinaryReader &reader) {}
 };
 #endif //  TRANSACTION_H
--- ontology-cplusplus-sdk/core/transaction/TransactionType.h
+++ ontology-cplusplus-sdk/core/transaction/TransactionType.h
@@ -1,13 +1,54 @@
+#ifndef TRANSACTIONTYPE_H
+#define TRANSACTIONTYPE_H
+
+#include "../../io/BinaryReader.h"
+#include "../../io/BinaryWriter.h"
+
 /**
  * list transaction types
  */
 enum TransactionType
 {
-    Bookkeeping = 0x00,
-    Bookkeeper = 0x02,
-    Claim = 0x03,
-    Enrollment = 0x04,
-    Vote = 0x05,
-    DeployCode = 0xd0,
-    InvokeCode = 0xd1,
+  Bookkeeping = 0x00,
+  Bookkeeper = 0x02,
+  Claim = 0x03,
+  Enrollment = 0x04,
+  Vote = 0x05,
+  DeployCode = 0xd0,
+  InvokeCode = 0xd1,
 };
+
+TransactionType TxTypeDeserialize(BinaryReader &reader)
+{
+  TransactionType TxType;
+  int val = reader.readByte();
+  switch (val)
+  {
+  case 0x00:
+    TxType = Bookkeeping;
+    break;
+  case 0x02:
+    TxType = Bookkeeper;
+    break;
+  case 0x03:
+    TxType = Claim;
+    break;
+  case 0x04:
+    TxType = Enrollment;
+    break;
+  case 0x05:
+    TxType = Vote;
+    break;
+  case 0xd0:
+    TxType = DeployCode;
+    break;
+  case 0xd1:
+    TxType = InvokeCode;
+    break;
+  default:
+    throw "IOException";
+  }
+  return TxType;
+}
+
+#endif // !TRANSACTIONTYPE_H
