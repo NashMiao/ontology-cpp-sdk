55e7f41820d0bc82ea838d08976099704648c96f
wdx7266 <wdx7266@vip.qq.com>
2018-06-05 17:29:57 +0800

    add Result.h

---
+++ ontology-cplusplus-sdk/network/restful/Result.h
@@ -0,0 +1,31 @@
+#ifndef RESULT_H
+#define RESULT_H
+
+#include <boost/any.hpp>
+#include <nlohmann/json.hpp>
+#include <string>
+
+struct struct_result {
+  std::string Action;
+  long long Error;
+  std::string Desc;
+  std::string Version;
+  std::string Result;
+};
+
+class Result {
+public:
+  struct_result result;
+  std::string toString() {
+    nlohmann::json json_result;
+    json_result["Action"] = result.Action;
+    json_result["Error"] = result.Error;
+    json_result["Desc"] = result.Error;
+    json_result["Version"] = result.Version;
+    json_result["Result"] = boost::any_cast<std::string>(result.Result);
+    std::string str_result = json_result.dump();
+    return str_result;
+  }
+};
+
+#endif // RESULT_H
