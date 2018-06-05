4b495e8937eda9a2b0f9c2447db3a90d108676f7
wdx7266 <wdx7266@vip.qq.com>
2018-06-05 18:16:13 +0800

    add Interfaces.h

---
+++ ontology-cplusplus-sdk/network/restful/Interfaces.h
@@ -0,0 +1,31 @@
+#ifndef INTERFACES_H
+#define INTERFACES_H
+
+#include <string>
+#include <unordered_map>
+
+class Interfaces {
+private:
+  std::string url;
+
+public:
+  Interfaces(std::string _url) { url = _url; }
+  std::string getUrl() { return url; }
+  std::string sendTransaction(bool preExec, std::string userid,
+                              std::string action, std::string version,
+                              std::string data) {
+    std::unordered_map<std::string, std::string> params;
+    if (userid != NULL) {
+      params.insert({std::string("userid"), userid});
+    }
+    if (preExec) {
+      params.insert({std::string("preExec"), std::string("1")});
+    }
+    std::unordered_map<std::string, std::string> body;
+    body.insert({"Action", action});
+    body.insert({"Version", version});
+    body.insert({"Data", data});
+  }
+};
+
+#endif
