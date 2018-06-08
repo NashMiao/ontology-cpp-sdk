b1e2992cb2bfe110b440da7f09401da5ce957ad0
wdx7266 <wdx7266@vip.qq.com>
2018-06-08 10:53:45 +0800

    update account

--- ontology-cplusplus-sdk/account/Account.h
+++ ontology-cplusplus-sdk/account/Account.h
@@ -1,14 +1,22 @@
 #ifndef ACCOUNT_H
 #define ACCOUNT_H
 
-#include "../crypto/Curve.h"
+#include "../common/Address.h"
+#include "../crypto/Sign.h"
 #include "../crypto/SignatureScheme.h"
-
 
 class Account {
 private:
   EVP_PKEY *PrivateKey;
   EVP_PKEY *PublicKey;
+  Address addressU160;
+  SignatureScheme signatureScheme;
+
+public:
+  Account(SignatureScheme scheme) {
+    addressU160 = Address.addressFromPubKey(serializePublicKey());
+  }
+  Address getAddressU160() { return addressU160; }
 };
 
 #endif
