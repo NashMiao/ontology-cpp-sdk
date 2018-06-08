faebcdab34afbff2222d312265721f72fa45fd5c
wdx7266 <wdx7266@vip.qq.com>
2018-06-08 16:19:32 +0800

    add VmType.h

---
+++ ontology-cplusplus-sdk/core/VmType.h
@@ -0,0 +1,23 @@
+#ifndef VMTYPE_H
+#define VMTYPE_H
+
+enum VmType { Native = 0xff, NEOVM = 0x80, WASMVM = 0x90 };
+
+VmType valueOf(unsigned char v) {
+  VmType ret;
+  switch (v) {
+  case 0xff:
+    ret = Native;
+    break;
+  case 0x80:
+    ret = NEOVM;
+  case 0x90:
+    ret = WASMVM;
+    break;
+  default:
+    throw "IllegalArgumentException";
+  }
+  return ret;
+}
+
+#endif
