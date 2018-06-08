e02387f319dfc351102d1cd78ed9d646cf3f926d
wdx7266 <wdx7266@vip.qq.com>
2018-06-08 10:54:24 +0800

    update common

--- ontology-cplusplus-sdk/common/Address.h
+++ ontology-cplusplus-sdk/common/Address.h
@@ -3,19 +3,56 @@
 
 #include "../io/BinaryReader.h"
 #include "../io/BinaryWriter.h"
+#include "Helper.h"
 #include <string>
+#include <vector>
 
 class Address
 {
-  private:
-    std::string ZERO;
-    unsigned char COIN_VERSION;
+private:
+  constexpr std::size_t zero_size;
+  std::vector<unsigned char> ZERO;
+  unsigned char COIN_VERSION_ONT;
+  unsigned char COIN_VERSIO_NEO;
 
-  public:
-    Address() { COIN_VERSION = 0x41; }
-    void deserialize(BinaryReader &reader){
-        ZERO=reader.readVarBytes();
+public:
+  Address()
+  {
+    zero_size = 20;
+    ZERO.reserve(zero_size);
+    COIN_VERSION_ONT = 0x41;
+    COIN_VERSIO_NEO = 0x17;
+  }
+
+  Address(const std::vector<unsigned char> &_zero)
+  {
+    zero_size = 20;
+    ZERO.reserve(zero_size);
+    if (_zero.size() != zero_size)
+    {
+      throw "Address Error: zero size error!"
     }
+    ZERO = _zero;
+  }
+
+  void set_zero(std::string str_zero)
+  {
+    memcpy(ZERO, (unsigned char *)str_zero.c_str(), 20);
+  }
+
+  void deserialize(BinaryReader &reader)
+  {
+    std::string str;
+    str = reader.readVarBytes();
+    memcpy(ZERO, (unsigned char *)str.c_str(), 20);
+  }
+
+  std::string toHexString()
+  {
+    std::vector<unsigned char> value;
+    Helper helper;
+    return helper.toHexString(value);
+  }
 };
 
 #endif // !ADDRESS_H
