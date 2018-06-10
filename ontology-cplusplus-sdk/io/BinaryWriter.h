9901b639ad0413f3c96672889bf0796bfec29f1e
wdx7266 <wdx7266@vip.qq.com>
2018-06-11 00:35:27 +0800

    update io

--- ontology-cplusplus-sdk/io/BinaryWriter.h
+++ ontology-cplusplus-sdk/io/BinaryWriter.h
@@ -11,37 +11,30 @@
 
 using namespace std;
 
-std::string myhexStr(unsigned char *data, int len)
-{
+std::string myhexStr(unsigned char *data, int len) {
   char hexmap[] = {'0', '1', '2', '3', '4', '5', '6', '7',
                    '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
   std::string s(len * 2, ' ');
-  for (int i = 0; i < len; ++i)
-  {
+  for (int i = 0; i < len; ++i) {
     s[2 * i] = hexmap[(data[i] & 0xF0) >> 4];
     s[2 * i + 1] = hexmap[data[i] & 0x0F];
   }
   return s;
 }
 
-class BinaryWriter
-{
+class BinaryWriter {
 private:
   std::ofstream outfile;
   std::vector<unsigned char> uc_vec;
 
-  void bytes_push_back(unsigned char *bytes, int len)
-  {
-    for (int i = 0; i < len; i++)
-    {
+  void bytes_push_back(unsigned char *bytes, int len) {
+    for (int i = 0; i < len; i++) {
       uc_vec.push_back(bytes[i]);
     }
   }
 
-  void long2bytes(const long long v, unsigned char *bytes, int bytes_len)
-  {
-    for (int i = 1; i < bytes_len; i++)
-    {
+  void long2bytes(const long long v, unsigned char *bytes, int bytes_len) {
+    for (int i = 1; i < bytes_len; i++) {
       bytes[i] = (v >> 8 * (8 - i)) & 0xFF;
     }
   }
@@ -49,33 +42,25 @@ private:
 public:
   BinaryWriter() { outfile.open("outfile", ios::out | ios::binary); }
   void set_ofstream(std::ofstream &ofstrm) { outfile.copyfmt(ofstrm); }
-  bool writeVarInt(long long v)
-  {
-    if (v < 0xFD)
-    {
+  bool writeVarInt(long long v) {
+    if (v < 0xFD) {
       unsigned char buffer = v & 0xFF;
       uc_vec.push_back(buffer);
-    }
-    else if (v <= 0xFFFF)
-    {
+    } else if (v <= 0xFFFF) {
       int bytes_len = 3;
       unsigned char *bytes = new unsigned char[bytes_len];
       long2bytes(v, bytes, bytes_len);
       bytes[0] = 0xFD;
       bytes_push_back(bytes, bytes_len);
       delete[] bytes;
-    }
-    else if (v <= 0xFFFFFFFF)
-    {
+    } else if (v <= 0xFFFFFFFF) {
       int bytes_len = 5;
       unsigned char *bytes = new unsigned char[bytes_len];
       long2bytes(v, bytes, bytes_len);
       bytes[0] = 0xFE;
       bytes_push_back(bytes, bytes_len);
       delete[] bytes;
-    }
-    else
-    {
+    } else {
       int bytes_len = 9;
       unsigned char *bytes = new unsigned char[bytes_len];
       long2bytes(v, bytes, bytes_len);
@@ -86,33 +71,39 @@ public:
     return true;
   }
 
-  bool writeVarBytes(const unsigned char *v)
-  {
+  bool writeByte(unsigned char v) { uc_vec.push_back(v); }
+
+  bool writeVarBytes(const unsigned char *v) {
     long long len = (sizeof(v) / sizeof(unsigned char));
 
-    if (!writeVarInt(len))
-    {
+    if (!writeVarInt(len)) {
       return false;
     }
 
-    for (int i = 0; i < len; i++)
-    {
+    for (int i = 0; i < len; i++) {
       uc_vec.push_back(v[i]);
     }
 
     return true;
   }
 
-  bool writeVarString(const std::string &str)
-  {
-    int str_len = str.length();
-    std::string utf8_str(str);
-    if (!writeVarInt(str_len))
-    {
+  bool writeVarBytes(std::vector<unsigned char> _vec) {
+    long long len = _vec.length();
+
+    if (!writeVarInt(len)) {
       return false;
     }
-    if (!writeVarBytes((unsigned char *)str.c_str()))
-    {
+    uc_vec.insert(uc_vec.cend(), _vec.cbegin(), _vec_cend());
+    return true;
+  }
+
+  bool writeVarString(const std::string &str) {
+    int str_len = str.length();
+    std::string utf8_str(str);
+    if (!writeVarInt(str_len)) {
+      return false;
+    }
+    if (!writeVarBytes((unsigned char *)str.c_str())) {
       return false;
     }
     return true;
---
+++ ontology-cplusplus-sdk/io/Serializable.h
@@ -0,0 +1,20 @@
+#ifndef SERIALIZABLE_H
+#define SERIALIZABLE_H
+
+#include "../common/Helper.h"
+#include "BinaryReader.h"
+#include "BinaryWriter.h"
+
+#include <vector>
+
+class Serializable {
+  virtual void serialize(BinaryWriter writer) = 0;
+  virtual void deserialize(BinaryReader &reader) = 0;
+  std::vector<unsigned char> toArray() {}
+  std::string toHexString() {
+    Helper helper;
+    helper.toHexString();
+  }
+};
+
+#endif
