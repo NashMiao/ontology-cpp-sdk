afb317d079d9566eef67ecf6dd873b6489d1a901
wdx7266 <wdx7266@vip.qq.com>
2018-06-15 13:42:26 +0800

    add Struct.h

---
+++ ontology-cplusplus-sdk/smartcontract/nativevm/abi/Struct.h
@@ -0,0 +1,20 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+#include <boost/any.hpp>
+#include <list>
+
+class Struct {
+private:
+  std::list<boost::any> List;
+
+public:
+  Struct add(std::list<boost::any> objs) {
+    std::list<boost::any>::iterator it;
+    for (it = objs.begin(); it != objs.end(); it++) {
+      List.push_back(*it);
+    }
+    return this;
+  }
+};
+#endif
