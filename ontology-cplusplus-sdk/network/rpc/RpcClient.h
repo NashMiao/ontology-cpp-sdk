9ea19a1f48e478f1a33379cfd9d7713606b78dfb
Nash <wdx7266@vip.qq.com>
2018-06-21 21:08:11 +0800

    update

--- ontology-cplusplus-sdk/network/rpc/RpcClient.h
+++ ontology-cplusplus-sdk/network/rpc/RpcClient.h
@@ -1,16 +1,32 @@
 #ifndef RPCCLIENT_H
 #define RPCCLIENT_H
 
+#include "../connect/IConnector.h"
+#include "RpcInterfaces.h"
 #include "boost/any.hpp"
+#include <iostream>
 #include <map>
 #include <vector>
 
-class RpcClient
-{
-  private:
+class RpcClient : public IConnector {
+private:
+  RpcInterfaces rpc;
 
-  public:
-  
+public:
+  RpcClient(std::string url) {
+    try {
+      rpc = new RpcInterfaces(url);
+    } catch (const char *err) {
+      std::cerr << err << std::endl;
+    }
+  }
+
+  std::string getUrl() { return rpc.getHost(); }
+
+  boost::any getBalance(std::string address) {
+    boost::any result;
+    result = rpc.call("getbalance", address);
+  }
 };
 
 #endif
--- ontology-cplusplus-sdk/network/rpc/RpcInterfaces.h
+++ ontology-cplusplus-sdk/network/rpc/RpcInterfaces.h
@@ -2,26 +2,55 @@
 #define RPCINTERFACES_H
 
 #include "boost/any.hpp"
+#include <curl/curl.h>
 #include <map>
 #include <string>
 #include <vector>
 
-class RpcInterfaces
-{
-  private:
-    std::string url;
+class RpcInterfaces {
+private:
+  std::string url;
+  
+  std::map<std::string, boost::any>
+  makeRequest(const std::string &method, std::vector<boost::any> params) {
+    std::map<std::string, boost::any> request = {
+        {"jsonrpc", "2.0"}, {"method", method}, {"params", params}, {"id", 1}};
+    std::string request_str = "POST url=" + url + ",";
+    std::cout << request_str << std::endl;
+    return request;
+  }
 
-  public:
-    RpcInterfaces(std::string _url) url(_url) {}
-    std::map makeRequest(std::string method, std::vector<boost::any> params)
-    {
-        std::map<std::string, boost::any> request = {
-            {"jsonrpc", "2.0"}, {"method", method}, {"params", params}, {"id", 1}};
-        std::string request_str = "POST url=" + url + ",";
-        std::cout << request_str << std::endl;
-        return request;
+public:
+  RpcInterfaces(std::string _url) url(_url) {}
+
+  std::string getHost() { return url; }
+
+  boost::any send(boost::any request, is_https = false) {
+    CURL *curl = curl_easy_init();
+    if (!curl) {
+      throw "RpcInterfaces send: curl_easy_init error!";
+    }
+    curl_slist *plist =
+        curl_slist_append(NULL, "Content-Type:application/json;charset=UTF-8");
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, plist);
+    if (is_https) {
+      curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, true);
+      curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, true);
     }
 
+    else {
+      curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
+      curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, false);
+    }
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+    curl_easy_setopt(curl, CURLOPT_READFUNCTION, NULL);
+  }
+
+  boost::any call(const std::string &method, boost::any params) {
+    std::map<std::string, boost::any> request;
+    request = makeRequest(method, params);
+  }
 };
 
 #endif
