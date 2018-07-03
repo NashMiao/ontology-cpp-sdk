702fed9292bd8353e7bb411d50c716245f9f6efa
Nash <wdx7266@vip.qq.com>
2018-07-03 19:03:42 +0800

    update

--- ontology-cplusplus-sdk/common/ErrorCode.h
+++ ontology-cplusplus-sdk/common/ErrorCode.h
@@ -6,179 +6,180 @@
 
 class ErrorCode
 {
-  public:
-    static std::string getError(int code, std::string msg)
+  private:
+    static nlohmann::json getError(int code, std::string msg)
     {
         nlohmann::json json_map = {{"Error", code}, {"Desc", msg}};
-        return json_map.dump();
+        return json_map;
     }
 
+  public:
     // account error
-    static std::string InvalidParams =
+    static nlohmann::json InvalidParams =
         getError(51001, "Account Error,invalid params");
-    static std::string UnsupportedKeyType =
+    static nlohmann::json UnsupportedKeyType =
         getError(51002, "Account Error,unsupported key type");
-    static std::string InvalidMessage =
+    static nlohmann::json InvalidMessage =
         getError(51003, "Account Error,invalid message");
-    static std::string WithoutPrivate = getError(
+    static nlohmann::json WithoutPrivate = getError(
         51004,
         "Account Error,account without private key cannot generate signature");
-    static std::string InvalidSM2Signature = getError(
+    static nlohmann::json InvalidSM2Signature = getError(
         51005,
         "Account Error,invalid SM2 signature parameter, ID (String) excepted");
-    static std::string AccountInvalidInput =
+    static nlohmann::json AccountInvalidInput =
         getError(51006, "Account Error,account invalid input");
-    static std::string AccountWithoutPublicKey = getError(
+    static nlohmann::json AccountWithoutPublicKey = getError(
         51007,
         "Account Error,account without public key cannot verify signature");
-    static std::string UnknownKeyType =
+    static nlohmann::json UnknownKeyType =
         getError(51008, "Account Error,unknown key type");
-    static std::string NullInput = getError(51009, "Account Error,null input");
-    static std::string InvalidData =
+    static nlohmann::json NullInput = getError(51009, "Account Error,null input");
+    static nlohmann::json InvalidData =
         getError(51010, "Account Error,invalid data");
-    static std::string Decoded3bytesError =
+    static nlohmann::json Decoded3bytesError =
         getError(51011, "Account Error,decoded 3 bytes error");
-    static std::string DecodePrikeyPassphraseError =
+    static nlohmann::json DecodePrikeyPassphraseError =
         getError(51012, "Account Error,decode prikey passphrase error.");
-    static std::string PrikeyLengthError =
+    static nlohmann::json PrikeyLengthError =
         getError(51013, "Account Error,Prikey length error");
-    static std::string EncryptedPriKeyError =
+    static nlohmann::json EncryptedPriKeyError =
         getError(51014, "Account Error,Prikey length error");
-    static std::string encryptedPriKeyAddressPasswordErr = getError(
+    static nlohmann::json encryptedPriKeyAddressPasswordErr = getError(
         51015, "Account Error,encryptedPriKey address password not match.");
-    static std::string EncriptPrivateKeyError =
+    static nlohmann::json EncriptPrivateKeyError =
         getError(51016, "Account Error, encript privatekey error,");
 
     //
-    static std::string ParamLengthErr =
+    static nlohmann::json ParamLengthErr =
         getError(52001, "Uint256 Error,param length error");
-    static std::string ChecksumNotValidate =
+    static nlohmann::json ChecksumNotValidate =
         getError(52002, "Base58 Error,Checksum does not validate");
-    static std::string InputTooShort =
+    static nlohmann::json InputTooShort =
         getError(52003, "Base58 Error,Input too short");
-    static std::string UnknownCurve =
+    static nlohmann::json UnknownCurve =
         getError(52004, "Curve Error,unknown curve");
-    static std::string UnknownCurveLabel =
+    static nlohmann::json UnknownCurveLabel =
         getError(52005, "Curve Error,unknown curve label");
-    static std::string UnknownAsymmetricKeyType =
+    static nlohmann::json UnknownAsymmetricKeyType =
         getError(52006, "keyType Error,unknown asymmetric key type");
-    static std::string InvalidSignatureData =
+    static nlohmann::json InvalidSignatureData =
         getError(52007, "Signature Error,invalid signature data: missing the ID "
                         "parameter for SM3withSM2");
-    static std::string InvalidSignatureDataLen =
+    static nlohmann::json InvalidSignatureDataLen =
         getError(52008, "Signature Error,invalid signature data length");
-    static std::string MalformedSignature =
+    static nlohmann::json MalformedSignature =
         getError(52009, "Signature Error,malformed signature");
-    static std::string UnsupportedSignatureScheme =
+    static nlohmann::json UnsupportedSignatureScheme =
         getError(52010, "Signature Error,unsupported signature scheme:");
-    static std::string DataSignatureErr =
+    static nlohmann::json DataSignatureErr =
         getError(52011, "Signature Error,Data signature error.");
-    static std::string UnSupportOperation =
+    static nlohmann::json UnSupportOperation =
         getError(52012, "Address Error, UnsupportedOperationException");
 
     // Core Error
-    static std::string TxDeserializeError =
+    static nlohmann::json TxDeserializeError =
         getError(53001, "Core Error,Transaction deserialize failed");
-    static std::string BlockDeserializeError =
+    static nlohmann::json BlockDeserializeError =
         getError(53002, "Core Error,Block deserialize failed");
 
     // merkle error
-    static std::string MerkleVerifierErr = getError(
+    static nlohmann::json MerkleVerifierErr = getError(
         54001, "Wrong params: the tree size is smaller than the leaf index");
-    static std::string TargetHashesErr = getError(54002, "targetHashes error");
+    static nlohmann::json TargetHashesErr = getError(54002, "targetHashes error");
 
-    static std::string ConstructedRootHashErr(String msg)
+    static nlohmann::json ConstructedRootHashErr(String msg)
     {
         return getError(54003, "Other Error," + msg);
     }
 
-    static std::string AsserFailedHashFullTree =
+    static nlohmann::json AsserFailedHashFullTree =
         getError(54004, "assert failed in hash full tree");
-    static std::string LeftTreeFull = getError(54005, "left tree always full");
+    static nlohmann::json LeftTreeFull = getError(54005, "left tree always full");
 
     // SmartCodeTx Error
-    static std::string SendRawTxError =
+    static nlohmann::json SendRawTxError =
         getError(58001, "SmartCodeTx Error,sendRawTransaction error");
-    static std::string TypeError =
+    static nlohmann::json TypeError =
         getError(58002, "SmartCodeTx Error,type error");
 
     // OntIdTx Error
-    static std::string NullCodeHash =
+    static nlohmann::json NullCodeHash =
         getError(58003, "OntIdTx Error,null codeHash");
-    static std::string ParamError = getError(58004, "param error,");
+    static nlohmann::json ParamError = getError(58004, "param error,");
 
-    static std::string ParamErr(String msg) { return getError(58005, msg); }
+    static nlohmann::json ParamErr(String msg) { return getError(58005, msg); }
 
-    static std::string DidNull = getError(
+    static nlohmann::json DidNull = getError(
         58006, "OntIdTx Error,SendDid or receiverDid is null in metaData");
-    static std::string NotExistCliamIssuer =
+    static nlohmann::json NotExistCliamIssuer =
         getError(58007, "OntIdTx Error,Not exist cliam issuer");
-    static std::string NotFoundPublicKeyId =
+    static nlohmann::json NotFoundPublicKeyId =
         getError(58008, "OntIdTx Error,not found PublicKeyId");
-    static std::string PublicKeyIdErr =
+    static nlohmann::json PublicKeyIdErr =
         getError(58009, "OntIdTx Error,PublicKeyId err");
-    static std::string BlockHeightNotMatch =
+    static nlohmann::json BlockHeightNotMatch =
         getError(58010, "OntIdTx Error,BlockHeight not match");
-    static std::string NodesNotMatch =
+    static nlohmann::json NodesNotMatch =
         getError(58011, "OntIdTx Error,nodes not match");
-    static std::string ResultIsNull =
+    static nlohmann::json ResultIsNull =
         getError(58012, "OntIdTx Error,result is null");
-    static std::string CreateOntIdClaimErr =
+    static nlohmann::json CreateOntIdClaimErr =
         getError(58013, "OntIdTx Error, createOntIdClaim error");
-    static std::string VerifyOntIdClaimErr =
+    static nlohmann::json VerifyOntIdClaimErr =
         getError(58014, "OntIdTx Error, verifyOntIdClaim error");
-    static std::string WriteVarBytesError =
+    static nlohmann::json WriteVarBytesError =
         getError(58015, "OntIdTx Error, writeVarBytes error");
-    static std::string SendRawTransactionPreExec =
+    static nlohmann::json SendRawTransactionPreExec =
         getError(58016, "OntIdTx Error, sendRawTransaction PreExec error");
-    static std::string SenderAmtNotEqPasswordAmt = getError(
+    static nlohmann::json SenderAmtNotEqPasswordAmt = getError(
         58017, "OntIdTx Error, senders amount is not equal password amount");
-    static std::string ExpireErr =
+    static nlohmann::json ExpireErr =
         getError(58017, "OntIdTx Error, expire is wrong");
-    static std::string GetStatusErr(String msg)
+    static nlohmann::json GetStatusErr(String msg)
     {
         return getError(58017, "GetStatus Error," + msg);
     };
 
     // OntAsset Error
-    static std::string AssetNameError =
+    static nlohmann::json AssetNameError =
         getError(58101, "OntAsset Error,asset name error");
-    static std::string DidError = getError(58102, "OntAsset Error,Did error");
-    static std::string NullPkId = getError(58103, "OntAsset Error,null pkId");
-    static std::string NullClaimId =
+    static nlohmann::json DidError = getError(58102, "OntAsset Error,Did error");
+    static nlohmann::json NullPkId = getError(58103, "OntAsset Error,null pkId");
+    static nlohmann::json NullClaimId =
         getError(58104, "OntAsset Error,null claimId");
-    static std::string AmountError = getError(
+    static nlohmann::json AmountError = getError(
         58105, "OntAsset Error,amount or gas is less than or equal to zero");
-    static std::string ParamLengthNotSame =
+    static nlohmann::json ParamLengthNotSame =
         getError(58105, "OntAsset Error,param length is not the same");
 
     // RecordTx Error
-    static std::string NullKeyOrValue =
+    static nlohmann::json NullKeyOrValue =
         getError(58201, "RecordTx Error,null key or value");
-    static std::string NullKey = getError(58202, "RecordTx Error,null  key");
+    static nlohmann::json NullKey = getError(58202, "RecordTx Error,null  key");
 
     // OntSdk Error
-    static std::string WebsocketNotInit =
+    static nlohmann::json WebsocketNotInit =
         getError(58301, "OntSdk Error,websocket not init");
-    static std::string ConnRestfulNotInit =
+    static nlohmann::json ConnRestfulNotInit =
         getError(58302, "OntSdk Error,connRestful not init");
 
     // abi error
-    static std::string SetParamsValueValueNumError =
+    static nlohmann::json SetParamsValueValueNumError =
         getError(58401, "AbiFunction Error,setParamsValue value num error");
-    static std::string ConnectUrlErr =
+    static nlohmann::json ConnectUrlErr =
         getError(58402, "Interfaces Error,connect error:");
 
-    static std::string ConnectUrlErr(std::string msg)
+    static nlohmann::json ConnectUrlErr(std::string msg)
     {
         return getError(58403, "connect error:" + msg);
     }
 
     // WalletManager Error
-    static std::string GetAccountByAddressErr =
+    static nlohmann::json GetAccountByAddressErr =
         getError(58501, "WalletManager Error,getAccountByAddress err");
 
-    static std::string OtherError(std::string msg)
+    static nlohmann::json OtherError(std::string msg)
     {
         return getError(59000, "Other Error," + msg);
     }
