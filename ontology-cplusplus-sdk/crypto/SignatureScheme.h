3748c58aecbe602df49f7d394302432a1d40aa3e
Nash <wdx7266@vip.qq.com>
2018-07-03 16:36:00 +0800

    add ordinal()

--- ontology-cplusplus-sdk/crypto/SignatureScheme.h
+++ ontology-cplusplus-sdk/crypto/SignatureScheme.h
@@ -1,7 +1,8 @@
 #ifndef SIGNATURESCHEME_H
 #define SIGNATURESCHEME_H
 
-enum class SignatureScheme {
+enum class SignatureScheme
+{
   SHA224withECDSA,
   SHA256withECDSA,
   SHA384withECDSA,
@@ -13,5 +14,47 @@ enum class SignatureScheme {
   RIPEMD160withECDSA,
   SM3withSM2
 };
+
+static int ordinal(SignatureScheme scheme)
+{
+  int ret;
+  switch (scheme)
+  {
+  case SignatureScheme::SHA224withECDSA:
+    ret = 0;
+    break;
+  case SignatureScheme::SHA256withECDSA:
+    ret = 1;
+    break;
+  case SignatureScheme::SHA384withECDSA:
+    ret = 2;
+    break;
+  case SignatureScheme::SHA512withECDSA:
+    ret = 3;
+    break;
+  case SignatureScheme::SHA3_224withECDSA:
+    ret = 4;
+    break;
+  case SignatureScheme::SHA3_256withECDSA:
+    ret = 5;
+    break;
+  case SignatureScheme::SHA3_384withECDSA:
+    ret = 6;
+    break;
+  case SignatureScheme::SHA3_512withECDSA:
+    ret = 7;
+    break;
+  case SignatureScheme::RIPEMD160withECDSA:
+    ret = 8;
+    break;
+  case SignatureScheme::SM3withSM2:
+    ret = 9;
+    break;
+  default:
+    throw "SignatureScheme Error!";
+    break;
+  }
+  return ret;
+}
 
 #endif // !SIGNATURESCHEME_H
