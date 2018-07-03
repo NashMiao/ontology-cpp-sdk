911387588c116fcd6ce80098544f698594dfaf6e
Nash <wdx7266@vip.qq.com>
2018-07-03 17:00:21 +0800

    add TX_MAX_SIG_SIZE TX_MAX_SIG_SIZE

--- ontology-cplusplus-sdk/common/Common.h
+++ ontology-cplusplus-sdk/common/Common.h
@@ -1,9 +1,12 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-class Common {
-public:
-   static constexpr const char * didont = "did:ont:";
+class Common
+{
+  public:
+    static constexpr const char *didont = "did:ont:";
+    static constexpr const int MULTI_SIG_MAX_PUBKEY_SIZE = 16;
+    static constexpr const int TX_MAX_SIG_SIZE = 16;
 };
 
 #endif
