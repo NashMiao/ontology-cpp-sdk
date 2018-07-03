36744aef5b44d6f93c5a0f3c0771101dacbafd34
Nash <wdx7266@vip.qq.com>
2018-07-03 17:00:58 +0800

    update

--- ontology-cplusplus-sdk/common/Common.h
+++ ontology-cplusplus-sdk/common/Common.h
@@ -5,8 +5,8 @@ class Common
 {
   public:
     static constexpr const char *didont = "did:ont:";
-    static constexpr const int MULTI_SIG_MAX_PUBKEY_SIZE = 16;
     static constexpr const int TX_MAX_SIG_SIZE = 16;
+    static constexpr const int MULTI_SIG_MAX_PUBKEY_SIZE = 16;
 };
 
 #endif
