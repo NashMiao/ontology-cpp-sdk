2833ca6773bf34f2d2a4e0fe7631cfa7c56e4cf2
Nash <wdx7266@vip.qq.com>
2018-07-04 21:09:15 +0800

    update

--- ontology-cplusplus-sdk/core/program/Program.h
+++ ontology-cplusplus-sdk/core/program/Program.h
@@ -8,6 +8,7 @@
 #include <openssl/ec.h>
 
 #include "../../common/Address.h"
+#include "../../common/Helper.h"
 #include "../../core/program/Program.h"
 #include "../../core/scripts/ScriptBuilder.h"
 #include "../../crypto/Curve.h"
@@ -18,38 +19,38 @@
 class Program
 {
   public:
-    static std::vector<unsigned char>
-    sortPublicKeys(const std::vector<std::string> &publicKeys)
+    static void sortPublicKeys(const std::vector<std::string> &publicKeys)
     {
-        int o1_label = getLabel(keyTypeFromPubkey(o1));
-        int o2_label = getLabel(keyTypeFromPubkey(o2));
-        if (o1_label != o2_label)
-        {
-            return (o1_label > o2_label ? 1 : -1);
-        }
-        std::sort(publicKeys.begin(), publicKeys.end(),
-                  [](std::string o1, std::string o2) -> int {
-                      int result;
-                      switch (keyTypeFromPubkey(o1))
-                      {
-                      case KeyType::SM2:
-                          // TODO
-                          break;
-                      case KeyType::ECDSA:
-                          EC_POINT *o1_ec_point;
-                          o1_ec_point = ECC::secp256r1.get_EC_Point(o1);
-                          EC_POINT *o2_ec_point;
-                          o2_ec_point = ECC::secp256r1.get_EC_Point(o2);
-                          result = ECC::secp256r1.compare(o1, o2);
-                          break;
-                      case KeyType::EDDSA:
-                          // TODO
-                          break;
-                      default:
-                          break;
-                      }
-                      return result;
-                  })
+        std::sort(publicKeys.begin(), publicKeys.end(), [](std::string o1, std::string o2) -> int {
+            int o1_label = getLabel(keyTypeFromPubkey(o1));
+            int o2_label = getLabel(keyTypeFromPubkey(o2));
+            if (o1_label != o2_label)
+            {
+                return (o1_label > o2_label ? 1 : -1);
+            }
+            int result;
+            switch (keyTypeFromPubkey(o1))
+            {
+            case KeyType::SM2:
+                // TODO
+                result = Helper::toHexString(o1).compare(Helper::toHexString(o2));
+                break;
+            case KeyType::ECDSA:
+                EC_POINT *o1_ec_point;
+                o1_ec_point = ECC::secp256r1.get_EC_Point(o1);
+                EC_POINT *o2_ec_point;
+                o2_ec_point = ECC::secp256r1.get_EC_Point(o2);
+                result = ECC::secp256r1.compare(o1, o2);
+                break;
+            case KeyType::EDDSA:
+                // TODO
+                break;
+            default:
+                result = Helper::toHexString(o1).compare(Helper::toHexString(o2));
+                break;
+            }
+            return result;
+        });
     }
 
     static std::vector<unsigned char>
@@ -62,6 +63,16 @@ class Program
         }
         ScriptBuilder builder;
         builder.push(m);
+        sortPublicKeys(publicKeys);
+        for (size_t i = 0; i < publicKeys.size(); i++)
+        {
+            builder.pushHexStr(publicKeys[i]);
+        }
+        BN_set_word(bn, (unsigned long)publicKeys.size());
+        builder.push(bn);
+        BN_clear(bn);
+        builder.add(ScriptOp::OP_CHECKMULTISIG);
+        return builder.toArray();
     }
 };
 
