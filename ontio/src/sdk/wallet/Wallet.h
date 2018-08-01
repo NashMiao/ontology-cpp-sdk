6994766bd185133f3239d0ec7330d110419b4c66
dingxin <wdx7266@vip.qq.com>
2018-08-01 03:27:27 -0700

    update

--- ontio/src/sdk/wallet/Wallet.h
+++ ontio/src/sdk/wallet/Wallet.h
@@ -8,27 +8,102 @@
 #include <string>
 #include <list>
 
+#include <nlohmann/json.hpp>
+
 #include "Identity.h"
+#include "ScryptParam.h"
 #include "AccountParam.h"
 
-class Wallet {
-    private:
-    std::string name;
-    std::string version;
-    std::string createTime;
-    std::string defaultOntid;
-    std::string defaultAccountAddress;
-    // Scrypt scrypt;
-    // Object extra;
-    std::list<Identity> identities;
-    std::list<AccountParame> accounts;
-    public:
-    Wallet(){
-        name = "com.github.ontio";
-        version = "1.0";
-        createTime = "";
-        defaultOntid = "";
-        defaultAccountAddress = "";
+class Wallet
+{
+private:
+  std::string name;
+  std::string version;
+  std::string createTime;
+  std::string defaultOntid;
+  std::string defaultAccountAddress;
+  ScryptParam scryptParam;
+  nlohmann::json extra;
+  std::list<Identity> identities;
+  std::list<AccountParame> accounts;
+
+public:
+  Wallet()
+  {
+    name = "com.github.ontio";
+    version = "1.0";
+  }
+
+      nlohmann::json getExtra() {
+        return extra;
+    }
+
+    void setExtra(nlohmann::json extra) {
+        extra = extra;
+    }
+
+    std::string getName() {
+        return name;
+    }
+
+    void setName(std::string name) {
+        name = name;
+    }
+
+    std::string getDefaultOntid() {
+        return defaultOntid;
+    }
+
+    void setDefaultOntid(std::string defaultOntid) {
+        defaultOntid = defaultOntid;
+    }
+
+    std::string getCreateTime() {
+        return createTime;
+    }
+
+    void setCreateTime(std::string createTime) {
+        createTime = createTime;
+    }
+
+    std::string getDefaultAccountAddress() {
+        return defaultAccountAddress;
+    }
+
+    void setDefaultAccountAddress(std::string defaultAccountAddress) {
+        defaultAccountAddress = defaultAccountAddress;
+    }
+
+    std::string getVersion() {
+        return version;
+    }
+
+    void setVersion(std::string version) {
+        version = version;
+    }
+
+    ScryptParam getScrypt() {
+        return scryptParam;
+    }
+
+    void setScrypt(ScryptParam scrypt) {
+        scrypt = scrypt;
+    }
+
+    std::list<Identity> getIdentities() {
+        return identities;
+    }
+
+    void setIdentities(std::list<Identity> identityList) {
+        identities = identityList;
+    }
+
+    std::list<AccountParame> getAccounts() {
+        return accounts;
+    }
+
+    void setAccounts(std::list<AccountParame> accountList) {
+        accounts = accountList;
     }
 };
 #endif // !WALLET_H
